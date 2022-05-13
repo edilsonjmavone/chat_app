e553ca6d4e6c6e77b4d0c6356ea3386b06366aca
edilsonjmavone <edil200jusi@gmail.com>
2022-05-13 18:26:19 +0200

    rooms system; type declarations; server state

--- src/app.ts
+++ src/app.ts
@@ -3,38 +3,127 @@ import express from "express";
 import socketio from "socket.io";
 import http from "http";
 import path from "path";
-import {} from "cors";
+import cors from "cors";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
 const app = express();
-
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3001;
 const server = http.createServer(app);
-
 const io = new socketio.Server(server, {
   cors: { origin: "*", credentials: true }
 });
-let usersCount = 0;
-
 app.use(express.static(path.join(__dirname, "./public/")));
+app.use(cors({ credentials: true }));
 
-io.on("connection", (socket: any) => {
-  console.log(`Connected socket: ${socket.id}`);
-  usersCount++;
-  io.emit("update", usersCount);
-  /**
-       * socket.on("user_ready", (data: any) =>{
-          socket.broadcast.emit('new_client', data)
-      } )
-       */
-  socket.on("msg", function(mesage: any) {
-    io.emit("hey", mesage);
-  });
+let usersCount = 0;
+type userDat = {
+  id: string;
+  name?: string;
+  room?: string | null;
+  online?: boolean;
+};
+type SocketMsgData = {
+  key: string;
+  message: string;
+  author: string;
+  to: string;
+};
+type MsgPayload = {
+  message: SocketMsgData;
+  isPrivate: Boolean;
+  room?: string;
+  socketId?: string;
+};
+let usersList: userDat[] = [];
 
-  socket.on("disconnect", (socket: any) => {
-    console.log(`disconnected socket: ${socket.id}`);
-    usersCount--;
-    io.emit("update", usersCount);
-  });
-});
+function findUser(userID: string): userDat | undefined {
+  return usersList.find(user => user.id == userID);
+}
+function addUser(userDat: userDat): void {
+  usersList.push(userDat);
+}
+function removeUser(userID: string): void {
+  usersList = usersList.filter(user => user.id == userID);
+}
+
+function updateUserData(
+  id: string,
+  name?: string,
+  room?: string | null,
+  online?: boolean
+) {
+  name && (usersList[usersList.findIndex(user => user.id == id)].name = name);
+  room && (usersList[usersList.findIndex(user => user.id == id)].room = room);
+  online &&
+    (usersList[usersList.findIndex(user => user.id == id)].online = online);
+}
+
+io.on(
+  "connection",
+  (
+    socket: socketio.Socket<
+      DefaultEventsMap,
+      DefaultEventsMap,
+      DefaultEventsMap,
+      any
+    >
+  ) => {
+    console.log(`Connected socket: ${socket.id}`);
+    addUser({ id: socket.id }); // setup server state
+    io.emit("update", { usersCount, usersList }); // emit server state to clients
+
+    function switchRomm(socketId: string, payload: any) {
+      // func 'findUser' can return 'undefined' in that case we wanna exit out
+      let user = findUser(socketId);
+      if (!user) return console.log("couldn't find user");
+      user.room && socket.leave(user.room);
+
+      socket.join(payload.roomID);
+      updateUserData(socketId, undefined, payload.roomID);
+      io.emit("update", { usersCount, usersList });
+      io.to(payload.roomID).emit("newUser", "new user joined");
+      io.emit("update", { usersCount, usersList });
+    }
+
+    socket.on("join room", (payload: any) => {
+      console.log(`Socket: ${socket.id} in room ${payload.roomID}`);
+      switchRomm(socket.id, payload);
+    });
+
+    socket.on("leave room", (payload: any) => {
+      socket.leave(payload.roomI);
+      console.log(`leave ${payload.roomI}`);
+    });
+
+    socket.on("msg", (payload: MsgPayload) => {
+      io.emit("eventMsg", payload);
+      if (payload.isPrivate) {
+        socket.to(payload.socketId!).emit("incomignMsg", payload.message);
+        console.log(
+          `${socket.id} says: ${payload.message.message} to ${payload.socketId}`
+        );
+        console.log(socket.rooms);
+      } else {
+        socket.to(payload.room!).emit("incomignMsg", payload.message);
+        console.log(
+          `${socket.id} says: ${payload.message.message} in ${payload.room}`
+        );
+        console.log(socket.rooms);
+      }
+    });
+    function err(error: any) {
+      //should be called when ever there is an internal server error
+      io.to(socket.id).emit("ERROR", error);
+    }
+
+    socket.on("disconnect", () => {
+      console.log(`Disconnected socket: ${socket.id}`);
+      removeUser(socket.id);
+      io.emit("update", { usersCount, usersList });
+    });
+  }
+);
+
+// setInterval(() => io.emit("update", { usersCount, usersList }), 4000);
 
 server.listen(port, () => console.log(`hello in port:${port}`));
--- src/public/script.js
+++ src/public/script.js
@@ -3,139 +3,40 @@ const url = window.location.href;
 const socket = io(url);
 const sub = document.querySelector("#submit");
 const cUserName = "user_name";
-//TODO:
-init();
+let room = "music";
+console.log(sub.value);
 
-function init() {
-  checkCookie();
-  checkChats();
-}
-
-function requestName() {
-  let userName = prompt("Enter your name!!");
-
-  if (userName == "" || userName == null) {
-    userName = "stranger";
-    alert(`Now your name is "${userName}"`);
-    document.querySelector("#msg").placeholder = `type as ${userName}`;
-    return userName;
-  } else {
-    // alert(`Now your name is "${userName}"`)
-    document.querySelector("#msg").placeholder = `type as ${userName}`;
-    return userName;
-  }
-}
-
-//EVENTS
-
-// WebSokeckt events
-socket.on("new_client", data => {
-  alert(`a new stranger is among us !!`);
+socket.on("connect", () => {
+  console.log("Connected");
+  // socket.emit("join room", { room, name: "Edilson" });
 });
 
-socket.on("hey", data => addMsg(data, true));
-
-socket.on("update", usersCount => {
-  document.querySelector("#count").innerText = `${String(
-    usersCount
-  )} users in this room`;
-});
-
-document.addEventListener("keydown", event => {
-  if (event.key == "Enter") {
-    send();
-  }
-});
-
-// METHODS>>>>>>
-function ready() {
-  socket.emit("user_ready", getCookie("user_name"));
-}
-function setCookie(name) {
-  console.log("seting cookies");
-  document.cookie = `user_name=${name}`;
-  // ready()
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+function joinRoom(roomI) {
+  // join new room
+  room = roomI;
+  socket.emit("join room", {
+    roomI,
+    name: "Edilson",
+    calback: (socketInstance, message) => {
+      socketInstance.emit("calback", message);
     }
-
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+  });
 }
 
-function checkCookie() {
-  console.log("checking cookies");
-  let username = getCookie("user_name");
-  if (username != "") {
-    alert("Welcome again " + username);
-    document.querySelector("#msg").placeholder = `type as ${username}`;
-    // ready()
-  } else {
-    username = requestName();
-    setCookie(username);
-  }
+socket.on("calback", message => {
+  console.log(message);
+});
+
+function leaveRoom(roomI) {
+  socket.emit("leave room", { roomI, name: "Edilson" });
 }
+let name = sub.textContent;
 
-function addMsg(data, store) {
-  let ele = document.createElement("li");
-  let br = document.createElement("br");
-  ele.textContent = `From ${data.author} \n ${data.message} `;
-  if (getCookie("user_name") == data.author)
-    ele.style.backgroundColor = "rgb(130, 199, 61)";
-  document.querySelector("#dataB").appendChild(ele);
-  document.querySelector("#dataB").appendChild(br);
-  if (store) {
-    chatMsgs.push(data);
-    storeChat();
-  }
-}
+socket.on("hey", message => {
+  console.log(message);
+});
 
-function send() {
-  let message = document.querySelector("#msg");
-
-  if (message) {
-    const data = {
-      key: Math.random(),
-      message: message.value,
-      author: getCookie("user_name")
-    };
-    socket.emit("msg", data);
-    message.value = "";
-    console.log(data);
-    return;
-  } else {
-    alert("type something!!!");
-    return;
-  }
-}
-
-function storeChat() {
-  sessionStorage.setItem("chats", JSON.stringify(chatMsgs));
-}
-
-function checkChats() {
-  const data = sessionStorage.getItem("chats");
-  if (data) {
-    chatMsgs.push = JSON.parse(data);
-    chatMsgs.forEach(element => {
-      addMsg(element, false);
-    });
-  } else {
-    console.log("there ins't a chat");
-  }
-}
-
-//Methods
-//"https://cdn.socket.io/3.1.3/socket.io.min.js"
+socket.on(() => console.log("server said NULL"));
+setInterval(() => {
+  // socket.emit("msg", { message: `[${name}]`, room });
+}, 4000);
